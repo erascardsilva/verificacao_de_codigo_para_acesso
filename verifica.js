363b1617f918903be8a9340a36c55bdba8b68b4c
Erasmo Cardoso <70297459+erascardsilva@users.noreply.github.com>
2023-02-24 10:48:32 -0300

    Add files via upload

---
+++ verifica.js
@@ -0,0 +1,37 @@
+/* Codigo para verificação de email enviado 
+        Erasmo Cardoso */
+
+ // bibliotecas       
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+//funções  
+ function gerarCodigo() {
+    const letrasNumeros = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let codigo = '';
+    
+    for (let i = 0; i < 4; i++) {
+      codigo += letrasNumeros.charAt(Math.floor(Math.random() * letrasNumeros.length));
+    }
+  
+    return codigo;
+  }
+ //variavel recebe codigo gerado aqui devera ser implantado envio do codigo por email para o cliente 
+  const codigo = gerarCodigo();
+  console.log('Código gerado:', codigo);
+ 
+  //esta entrada da confirmação 
+  readline.question('Digite o codigo : ', input => {
+    const veri_email = input;
+  //Acesso
+    if (codigo === veri_email){
+      console.log('Acesso Garantido');
+    } else {
+      console.log('Tente novamente');
+    }
+  
+    readline.close();
+  });
+  
